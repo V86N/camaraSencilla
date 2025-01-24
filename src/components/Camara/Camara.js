78c07262566d65b0dad793196041c1c5613309b6
V86N <victor.nap86@gmail.com>
2025-01-24 11:29:58 +0100

    camara trasera implementada

--- src/components/Camara/Camara.js
+++ src/components/Camara/Camara.js
@@ -1,36 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Webcam from "react-webcam";
 
 export default class Camara extends Component {
-    setRef=webcam=>{
-        this.webcam=webcam;
-    }
+  setRef = webcam => {
+    this.webcam = webcam;
+  };
 
-        state={
-            imagen:null
-        }
+  state = {
+    imagen: null
+  };
 
-         foto=()=>{
-             var captura=this.webcam.getScreenshot();
-             console.log(captura)
-             this.setState({
-                 imagen:captura  
-             })
-         };
+  foto = () => {
+    const captura = this.webcam.getScreenshot();
+    console.log(captura);
+    this.setState({
+      imagen: captura
+    });
+  };
 
-    render() {
+  render() {
+    // Configuración para usar la cámara trasera
+    const videoConstraints = {
+      facingMode:  "environment"  // Selecciona la cámara trasera
+    };
 
-        return (
-            <div className='App'>
-                <Webcam audio={false} height={350} ref={this.setRef} screenshotFormat="image/jpeg" width={350}/>
-                 <br/> 
-                <button onClick={this.foto}>Hacer captura</button>
-                <hr/>
-                <img src={this.state.imagen} alt=""/>  
-
-                <br/>
-                <a href={this.state.imagen} download="captura">Descargar captura</a> 
-            </div>
-        )
-    }
+    return (
+      <div className='App'>
+        <Webcam
+          audio={false}
+          height={350}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={350}
+          videoConstraints={videoConstraints} // Aplica los constraints
+        />
+        <br />
+        <button onClick={this.foto}>Hacer captura</button>
+        <hr />
+        {this.state.imagen && (
+          <>
+            <img src={this.state.imagen} alt="Captura" />
+            <br />
+            <a href={this.state.imagen} download="captura">Descargar captura</a>
+          </>
+        )}
+      </div>
+    );
+  }
 }
